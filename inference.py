d4172187f06b948d75104bc48954013375c470ba
hoangnguyen2003 <ninjadoc0152@gmail.com>
2024-10-23 13:48:55 +0700

    change config

--- inference.py
+++ inference.py
@@ -22,7 +22,7 @@ pipe = DiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16)
 pipe.to("cuda")
 pipe.load_lora_weights(os.path.join(model_path, f"checkpoint-{args.checkpointing_steps * args.num_train_epochs}"))
 
-prompt_postfix = " sitting on a rocket."
+prompt_postfix = " sitting on a rocket"
 image_postfix = prompt_postfix.replace(" ", "_")
 
 # create folder
@@ -30,6 +30,6 @@ output_folder = f"./inference_results/{args.character_name}"
 os.makedirs(output_folder)
 
 # remember to use the place holader here
-prompt = f"A photo of {args.placeholder_token}{prompt_postfix}."
+prompt = f"A sticker of {args.placeholder_token}{prompt_postfix}"
 image = pipe(prompt, num_inference_steps=35, guidance_scale=7.5).images[0]
 image.save(os.path.join(output_folder, f"{args.character_name}_{image_postfix}.png"))
